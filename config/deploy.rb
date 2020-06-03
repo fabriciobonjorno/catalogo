cd9559ebf2358e4840c159dc0e2867a0f6963121
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2020-06-03 08:17:09 -0400

    Adjust database

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,8 +39,8 @@ namespace :deploy do
   desc "Make sure local git is in sync with remote."
   task :check_revision do
     on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/deploy`
-        puts "WARNING: HEAD is not the same as origin/deploy"
+      unless `git rev-parse HEAD` == `git rev-parse origin/master`
+        puts "WARNING: HEAD is not the same as origin/master"
         puts "Run `git push` to sync changes."
         exit
       end
