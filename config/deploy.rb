90ad18d6956c04738a21c49b0e022b72f40f9a34
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2020-06-02 22:32:34 -0400

    Adjust credentials

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,7 @@ set :repo_url, "git@github.com:fabriciobonjorno/catalogo.git"
 
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
+set :linked_files, %w{config/master.key}
 # Don't change these unless you know what you're doing
 set :pty,             true
 set :use_sudo,        false
