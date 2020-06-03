77d4eeea0bed06cd1060811a42170fb8b5052dff
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2020-06-03 14:44:48 -0400

    Adjust capstrano seed

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 set :branch, 'master'
-set :server_address, 'meucatalogo.art.br'
+set :server_address, '45.35.104.16'
 
 ask(:password, nil, echo: false)
 server fetch(:server_address), user: "deploy", roles: %w{app db web}
