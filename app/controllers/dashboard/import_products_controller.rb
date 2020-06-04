eba2dce68a15faee1ee43981be5f23beb6bb81dd
Andre Oliveira <desmonplays@gmail.com>
2020-06-03 20:42:57 -0400

    (new feature) executing service and reload page

---
+++ app/controllers/dashboard/import_products_controller.rb
@@ -0,0 +1,18 @@
+module Dashboard
+  class ImportProductsController < DashboardController
+    def create
+      GeneratePdfMaterialsRecordByCsv.execute(
+        import_products_params[:upload]
+      )
+      redirect_to dashboard_products_path,
+        notice: 'Produto(s) cadastrado(s) com sucesso'
+    end
+
+    private
+
+    def import_products_params
+      params.require(:spreadsheet).permit(:upload)
+    end
+  end
+end
+
