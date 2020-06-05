7f9c8b65db88e5a7389025fe1c34e3677cd596e3
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2020-06-05 09:45:22 -0400

    Config s3 active storage

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,8 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   #config.active_storage.service = :amazon
-  config.active_storage.service = :local
+  #config.active_storage.service = :local
+  config.active_storage.service = :amazon
   config.require_master_key = true
 
   # Mount Action Cable outside main process or domain
