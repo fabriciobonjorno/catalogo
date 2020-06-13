32e1b727eb8a7a430d829a17c83e9a2a798594f1
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2020-06-13 14:32:14 -0400

    Adjust paginate for families

--- app/controllers/family_details_controller.rb
+++ app/controllers/family_details_controller.rb
@@ -2,7 +2,6 @@ class FamilyDetailsController < HomeController
   #include MenuResources
   #before_action :set_resources, only: %i[show]
   def show
-    @family = Family.find(params[:id])
-    @products = Product.order(:product_code).page(params[:page]).per(9)   
+    @family = Family.page(params[:page]).per(9).find(params[:id])
   end
 end
