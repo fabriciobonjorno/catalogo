e200fea26bb614f669a513321273fada6373786b
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2020-06-13 16:15:14 -0400

    Adjust paginate for families

--- app/controllers/product_details_controller.rb
+++ app/controllers/product_details_controller.rb
@@ -2,7 +2,7 @@ class ProductDetailsController < HomeController
   #include MenuResources
   #before_action :set_resources, only: %i[show]
   def show
-    @product = Product.friendly.find(params[:id])
+    @product = Product.limit(9).friendly.find(params[:id])
     @product_dimension = Product.status_dimension
 
     #@similar_products = @product.parent&.sub_products.where.not(id: @product.id)
