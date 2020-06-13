ef69ec40b58eb869fe5f47e0a2229a76fc0f0a43
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2020-06-13 12:19:34 -0400

    Config paginate family_details

--- app/controllers/family_details_controller.rb
+++ app/controllers/family_details_controller.rb
@@ -3,5 +3,6 @@ class FamilyDetailsController < HomeController
   #before_action :set_resources, only: %i[show]
   def show
     @family = Family.find(params[:id])
+    @products = Product.order(:product_code).page(params[:page]).per(9)   
   end
 end
