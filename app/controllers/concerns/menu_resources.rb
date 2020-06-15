f60907d051f71489e201e4c10430aa6d4a7a50aa
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2020-06-15 11:08:56 -0400

    Adjust order menu group and family

--- app/controllers/concerns/menu_resources.rb
+++ app/controllers/concerns/menu_resources.rb
@@ -6,7 +6,8 @@ module MenuResources
   private
 
   def set_resources
-    @lines = Line.order_family
+    @lines = Line.all
+    @families = Family.order_family
     @groups = Group.order_group
   end
 end
