2b707252e0669ee5075bc7592f6437ca695b82ea
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2020-06-16 11:06:50 -0400

    Adjust scope group and family

--- app/models/family.rb
+++ app/models/family.rb
@@ -5,5 +5,5 @@ class Family < ApplicationRecord
   belongs_to :group
   has_many :products, dependent: :destroy
 
-  default_scope -> { order(family_description: :asc)}
+  default_scope { order(family_description: :asc)}
 end
