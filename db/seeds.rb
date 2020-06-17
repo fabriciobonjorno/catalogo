18775c901626d9f4cbe053f0e2c1ebb4ebfc7d99
Lucas Caton <lucascaton@gmail.com>
2020-06-17 17:38:57 +1000

    Uncomment DB seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,52 +18,52 @@ end
 
 # ------------------------------------------------------------------------------
 
-# puts ">> Creating groups..."
+puts ">> Creating groups..."
 
-# group_mapping = {
-#   "Nestlé" => ["Produto Nestlé"],
-#   "Food" => ["Produto Food"]
-# }
+group_mapping = {
+  "Nestlé" => ["Produto Nestlé"],
+  "Food" => ["Produto Food"]
+}
 
-# group_mapping.each.with_index(1) do |(line_description, group_descriptions), index|
-#   group_descriptions.each do |group_description|
-#     Group.find_or_create_by!(group_code: index) do |group|
-#       group.line = Line.find_by(line_description: line_description)
-#       group.group_description = group_description
-#     end
-#   end
-# end
+group_mapping.each.with_index(1) do |(line_description, group_descriptions), index|
+  group_descriptions.each do |group_description|
+    Group.find_or_create_by!(group_code: index) do |group|
+      group.line = Line.find_by(line_description: line_description)
+      group.group_description = group_description
+    end
+  end
+end
 
-# # ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
-# puts ">> Creating families..."
+puts ">> Creating families..."
 
-# Group.all.each.with_index(1) do |group, index|
-#   Family.find_or_create_by!(family_code: index) do |family|
-#     family.group = group
-#     family.family_description = group.group_description.gsub(/Produto/, "Família")
-#   end
-# end
+Group.all.each.with_index(1) do |group, index|
+  Family.find_or_create_by!(family_code: index) do |family|
+    family.group = group
+    family.family_description = group.group_description.gsub(/Produto/, "Família")
+  end
+end
 
-# # ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
-# puts ">> Creating tax classifications..."
+puts ">> Creating tax classifications..."
 
-# tax_classification = TaxClassification.find_or_create_by!(tax_code: "12.214.201.00")
+tax_classification = TaxClassification.find_or_create_by!(tax_code: "12.214.201.00")
 
-# # ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
-# puts ">> Creating products..."
+puts ">> Creating products..."
 
-# Family.all.each.with_index(1) do |family, index|
-#   Product.find_or_create_by!(product_code: index) do |product|
-#     product.group = family.group
-#     product.family = family
-#     product.tax_classification = tax_classification
-#     product.product_description = family.group.group_description
-#     product.product_ean = index
-#     product.product_dun = index
-#     product.status = true
-#     product.releases = true
-#   end
-# end
+Family.all.each.with_index(1) do |family, index|
+  Product.find_or_create_by!(product_code: index) do |product|
+    product.group = family.group
+    product.family = family
+    product.tax_classification = tax_classification
+    product.product_description = family.group.group_description
+    product.product_ean = index
+    product.product_dun = index
+    product.status = true
+    product.releases = true
+  end
+end
