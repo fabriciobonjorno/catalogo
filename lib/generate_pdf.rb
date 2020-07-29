404c1189174755ac452484709caa4a4d188f0ac7
Andre Oliveira <desmonplays@gmail.com>
2020-07-29 20:19:26 -0300

    feature: trying to not use url on images

--- lib/generate_pdf.rb
+++ lib/generate_pdf.rb
@@ -30,7 +30,7 @@ class GeneratePdf < Prawn::Document
     set_title(pdf, products.first)
     products.each do |product|
       pdf.bounding_box([@x, @y], width: 130, height: 800) do
-        pdf.image product.image_url, scale: 0.50 if product&.image&.attached?
+        pdf.image product.image, scale: 0.50 if product&.image&.attached?
         pdf.text product.product_description, size: 12, color: 'FF0000'
         pdf.text "Cód #{product.product_code}", size: 12, color: '0C71E0'
 
