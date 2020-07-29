f11b56d0a0efd83baa7ab2ef0e9a37b05cd6e475
Andre Oliveira <desmonplays@gmail.com>
2020-07-29 20:28:26 -0300

    feature: loading service url

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -3,8 +3,4 @@
 class ApplicationRecord < ActiveRecord::Base
   include Rails.application.routes.url_helpers
   self.abstract_class = true
-
-  def default_url_options 
-    { host: "brokermatogrosso.meucatalogo.art.br"} if Rails.env.production?
-  end
 end
